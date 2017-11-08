5758349d839c59c728b8a33c390f2b4173efcc36
JTorrus <jtorrusgalindo@gmail.com>
2017-11-08 20:23:51 +0100

    Class changed to data class, we just need to store attributes

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
@@ -4,4 +4,4 @@ package com.example.alumne.listviews_adapters_jtorrus.model
  * Created by Alumne on 08/11/2017.
  */
 
-data class ItemModel (private var name: String, private var s: Int, private var q: Int, private var i: Int)
+data class ItemModel (private var name: String, private var stock: Int, private var quantity: Int, private var image: Int)
