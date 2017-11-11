1928de09da27276a4853ac13a8090e29495387d1
JTorrus <jtorrusgalindo@gmail.com>
2017-11-11 14:15:58 +0100

    Restructured ItemModel class, all CustomAdapter methods implemented

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/CustomAdapter.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/CustomAdapter.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import com.example.alumne.listviews_adapters_jtorrus.R.id.image_of_item
 import com.example.alumne.listviews_adapters_jtorrus.model.ItemModel
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -13,7 +15,7 @@ import kotlinx.android.synthetic.main.activity_main.view.*
  * Created by Alumne on 08/11/2017.
  */
 
-class CustomAdapter (private var activity: Activity, private var data: List<ItemModel>, private var item_layout: Int): BaseAdapter() {
+class CustomAdapter (activity: Activity, val data: List<ItemModel>, private val item_layout: Int): BaseAdapter() {
     private var inflater: LayoutInflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
@@ -22,6 +24,12 @@ class CustomAdapter (private var activity: Activity, private var data: List<Item
         if (p1 == null) {
             view = inflater.inflate(item_layout, null)
         }
+
+        p1?.image_of_item?.setImageResource(data[p0].image)
+        p1?.name_of_item?.text = data[p0].name
+        p1?.stock_of_item?.text = data[p0].stock.toString()
+
+        return p1!!
     }
 
     override fun getItem(p0: Int): Any {
--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
@@ -4,4 +4,4 @@ package com.example.alumne.listviews_adapters_jtorrus.model
  * Created by Alumne on 08/11/2017.
  */
 
-data class ItemModel (private var name: String, private var stock: Int, private var quantity: Int, private var image: Int)
+data class ItemModel (val name: String, val stock: Int, val quantity: Int, val image: Int)
