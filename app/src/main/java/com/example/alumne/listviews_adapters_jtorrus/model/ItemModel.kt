45e5d420a31f6bcfc22764ff2171eddfb55e64aa
JTorrus <jtorrusgalindo@gmail.com>
2017-11-12 15:11:16 +0100

    Removed unnecessary comments.

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/ItemModel.kt
@@ -1,7 +1,3 @@
 package com.example.alumne.listviews_adapters_jtorrus.model
 
-/**
- * Created by Alumne on 08/11/2017.
- */
-
 data class ItemModel (val name: String, val stock: Int, val image: Int)
