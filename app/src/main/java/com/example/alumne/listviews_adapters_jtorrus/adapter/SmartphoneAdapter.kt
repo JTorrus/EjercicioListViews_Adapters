827121ef6a03ea5f1df97abfea2b895f1f0b4d1c
JTorrus <jtorrusgalindo@gmail.com>
2017-11-19 01:41:08 +0100

    Content to spinner added for each product. Classes renamed to Smartphone and SmartphoneAdapter.

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/SmartphoneAdapter.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/SmartphoneAdapter.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.Spinner
 import com.example.alumne.listviews_adapters_jtorrus.R
-import com.example.alumne.listviews_adapters_jtorrus.model.ItemModel
+import com.example.alumne.listviews_adapters_jtorrus.model.Smartphone
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class CustomAdapter (context: Context, val data: List<ItemModel>): BaseAdapter() {
+class SmartphoneAdapter(private var context: Context, private var data: ArrayList<Smartphone>): BaseAdapter() {
     private var inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
+        val adapter = ArrayAdapter<Int>(context, android.R.layout.simple_spinner_dropdown_item)
 
         if (view == null) {
             view = inflater.inflate(R.layout.list_item, null)
@@ -22,6 +25,12 @@ class CustomAdapter (context: Context, val data: List<ItemModel>): BaseAdapter()
         view?.image_of_item?.setImageResource(data[position].image)
         view?.name_of_item?.text = data[position].name
         view?.stock_of_item?.text = data[position].stock.toString()
+
+        for (key in 0..data[position].stock) {
+            adapter.add(key)
+        }
+
+        view?.qty_to_buy?.adapter = adapter
 
         return view!!
     }
@@ -37,5 +46,4 @@ class CustomAdapter (context: Context, val data: List<ItemModel>): BaseAdapter()
     override fun getCount(): Int {
         return data.size
     }
-
 }
---
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/Smartphone.kt
@@ -0,0 +1,3 @@
+package com.example.alumne.listviews_adapters_jtorrus.model
+
+data class Smartphone(var name: String, var stock: Int, var image: Int)
