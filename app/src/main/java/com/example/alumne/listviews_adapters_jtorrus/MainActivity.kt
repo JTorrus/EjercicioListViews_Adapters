ae4f973348d849945e323b8cf232a4c369b4e3c8
JTorrus <jtorrusgalindo@gmail.com>
2017-11-19 21:54:26 +0100

    OnActivityResult and nItemClick methods correctly implemented.

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/MainActivity.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.alumne.listviews_adapters_jtorrus
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
 import android.widget.AdapterView
 import android.widget.Toast
-import com.example.alumne.listviews_adapters_jtorrus.adapter.CustomAdapter
-import com.example.alumne.listviews_adapters_jtorrus.model.ItemModel
+import com.example.alumne.listviews_adapters_jtorrus.adapter.SmartphoneAdapter
+import com.example.alumne.listviews_adapters_jtorrus.model.Smartphone
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener{
-    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        Toast.makeText(this, "${p2}", Toast.LENGTH_LONG).show()
-    }
+    val SELECTED_QUANTITY = "user_input"
+    val SELECTED_ELEMENT = "element_selected"
+    private lateinit var data: ArrayList<Smartphone>
 
-    private lateinit var data: ArrayList<ItemModel>
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val intent = Intent(this@MainActivity, SelectedActivity::class.java)
+
+        val quantity = p1?.qty_to_buy?.selectedItem as Int
+        val element: Smartphone = p0?.getItemAtPosition(p2) as Smartphone
+
+        intent.putExtra(SELECTED_QUANTITY, quantity)
+        intent.putExtra(SELECTED_ELEMENT, element)
+
+        startActivityForResult(intent, 0)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +36,40 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener{
 
         createDataModel()
 
-        listView.adapter = CustomAdapter(this, data)
-
+        listView.adapter = SmartphoneAdapter(this, data)
         listView.onItemClickListener = this
+
+        button_to_confirm.visibility = GONE
+        button_to_cancel.visibility = GONE
     }
 
-    fun createDataModel() {
-        data = ArrayList<ItemModel>()
+    private fun createDataModel() {
+        data = ArrayList<Smartphone>()
 
         val namesOfItems: Array<String> = this.resources.getStringArray(R.array.names_of_items)
         val stocksOfItems: IntArray = this.resources.getIntArray(R.array.stocks_of_items)
 
-        var item: ItemModel
+        var item: Smartphone
 
         for (i in 0 until namesOfItems.size) {
-            item = ItemModel(namesOfItems[i], stocksOfItems[i], this.resources.getIdentifier("smartphone", "drawable", this.packageName))
+            item = Smartphone(namesOfItems[i], stocksOfItems[i], this.resources.getIdentifier(this.resources.getString(R.string.name_of_images), this.resources.getString(R.string.name_type_of_images), this.packageName))
             data.add(item)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            val messageOk = resources.getString(R.string.activity_messageOK)
+
+            Toast.makeText(applicationContext, messageOk, Toast.LENGTH_LONG).show()
+        } else {
+            val messageKo = resources.getString(R.string.activity_messageKO)
+
+            Toast.makeText(applicationContext, messageKo, Toast.LENGTH_LONG).show()
+        }
+    }
 }
+
+
