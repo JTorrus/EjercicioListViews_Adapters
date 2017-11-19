116f3fe003972b42edc8fdfb8e03b2899737b9a7
JTorrus <jtorrusgalindo@gmail.com>
2017-11-19 23:28:22 +0100

    Comments added.

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/SmartphoneAdapter.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/adapter/SmartphoneAdapter.kt
@@ -11,6 +11,11 @@ import com.example.alumne.listviews_adapters_jtorrus.R
 import com.example.alumne.listviews_adapters_jtorrus.model.Smartphone
 import kotlinx.android.synthetic.main.list_item.view.*
 
+/**
+ * He utilizado un adapter personalizado ya que en este caso los Adapters que vienen por defecto no son lo suficientemente potentes como para poder trabajar con los datos que tratamos
+ * en el ejercicio. Implemento el BaseAdapter ya que es el adapter general del que nacen todos y permite crear uno a medida como el que uso.
+ */
+
 class SmartphoneAdapter(internal var context: Context, internal var data: ArrayList<Smartphone>): BaseAdapter() {
     private var inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     internal var quantity: Int = 0
@@ -21,6 +26,10 @@ class SmartphoneAdapter(internal var context: Context, internal var data: ArrayL
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
+        /**
+         * Para el adapter del Spinner he usado un ArrayAdapter ya que recorreré una cantidad de posibles valores que le daré al usuario para que pueda elegir. En este caso será desde 0 hasta el stock
+         * disponible de cada producto.
+         */
         val adapter = ArrayAdapter<Int>(context, android.R.layout.simple_spinner_dropdown_item)
 
         if (view == null) {
