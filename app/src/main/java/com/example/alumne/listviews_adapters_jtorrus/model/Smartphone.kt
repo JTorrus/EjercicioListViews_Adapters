80ef1cb1255f74d52d3ceddac334c1e64db625e3
JTorrus <jtorrusgalindo@gmail.com>
2017-11-19 17:31:52 +0100

    Parcelable interface implemented.

--- app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/Smartphone.kt
+++ app/src/main/java/com/example/alumne/listviews_adapters_jtorrus/model/Smartphone.kt
@@ -1,3 +1,32 @@
 package com.example.alumne.listviews_adapters_jtorrus.model
 
-data class Smartphone(var name: String, var stock: Int, var image: Int)
+import android.os.Parcel
+import android.os.Parcelable
+
+class Smartphone(var name: String, var stock: Int, var image: Int): Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readInt()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeInt(stock)
+        parcel.writeInt(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Smartphone> {
+        override fun createFromParcel(parcel: Parcel): Smartphone {
+            return Smartphone(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Smartphone?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
